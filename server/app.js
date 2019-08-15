1a8528f05c35e84b30abcac4b78ff7099c96af17
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-15 23:59:07 +0400

    add: delete post graphql schema, resolvers and FE handler

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const graphqlHttp = require("express-graphql");
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
 const auth = require("./middleware/auth");
+const { clearImage } = require("./util/file");
 
 const app = express();
 
@@ -104,8 +105,3 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
-
-const clearImage = filePath => {
-  filePath = path.join(__dirname, "..", filePath);
-  fs.unlink(filePath, err => console.log(err));
-};
